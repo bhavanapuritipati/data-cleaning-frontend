c9ef7c0e68470265ab8182e0530002c8141be491
bhavanapuritipati <puritipatibhavana@gmail.com>
2025-12-08 16:37:36 +0530

    dependencies and app prerequisites

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <div className="antialiased text-gray-900 bg-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
